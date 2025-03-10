7704be994af43402ae54b6f811f36362d75054cf
ahafidi <ahafidi.dev@gmail.com>
2025-03-10 11:11:56 +0100

    add the edit markdown tab

--- components/dashboard/article-canvas.tsx
+++ components/dashboard/article-canvas.tsx
@@ -7,13 +7,14 @@ import ReactMarkdown from 'react-markdown'
 import rehypeRaw from 'rehype-raw'
 import rehypeSanitize from 'rehype-sanitize'
 // import remarkBreaks from 'remark-breaks'
-import { Skeleton } from '@/components/ui/skeleton'
+import { Tabs, TabsContent } from '@/components/ui/tabs'
+import { Textarea } from '@/components/ui/textarea'
 import remarkGfm from 'remark-gfm'
 import { ArticleOptions } from './article-options'
 
 export function ArticleCanvas() {
-  const { article, isLoading } = usePanel()
-
+  const { article, isLoading, updateArticle } = usePanel()
+  const [activeTab, setActiveTab] = useState('preview')
   const [isClient, setIsClient] = useState(false)
 
   const scrollableDivRef = useRef<HTMLDivElement>(null)
@@ -29,86 +30,100 @@ export function ArticleCanvas() {
     }
   }, [article, isLoading])
 
+  const handleMarkdownChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    updateArticle(e.target.value)
+  }
+
   return (
     <main className="h-full flex flex-col p-4">
-      <ArticleOptions />
+      <ArticleOptions
+        activeTab={activeTab}
+        onTabChange={setActiveTab}
+        isLoading={isLoading}
+      />
 
       <div className="flex-1 overflow-y-auto mt-3" ref={scrollableDivRef}>
-        {isClient && article ? (
-          <article className="prose prose-slate dark:prose-invert max-w-none">
-            <ReactMarkdown
-              remarkPlugins={[remarkGfm]}
-              rehypePlugins={[rehypeRaw, rehypeSanitize]}
-              components={{
-                h1: ({ node, ...props }) => (
-                  <h1
-                    className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl"
-                    {...props}
-                  />
-                ),
-                h2: ({ node, ...props }) => (
-                  <h2
-                    className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0"
-                    {...props}
-                  />
-                ),
-                h3: ({ node, ...props }) => (
-                  <h3
-                    className="scroll-m-20 text-2xl font-semibold tracking-tight"
-                    {...props}
-                  />
-                ),
-                h4: ({ node, ...props }) => (
-                  <h4
-                    className="scroll-m-20 text-xl font-semibold tracking-tight"
-                    {...props}
-                  />
-                ),
-                p: ({ node, ...props }) => (
-                  <p
-                    className="leading-7 [&:not(:first-child)]:mt-6"
-                    {...props}
-                  />
-                ),
-                ul: ({ node, ...props }) => (
-                  <ul className="my-6 ml-6 list-disc [&>li]:mt-2" {...props} />
-                ),
-                ol: ({ node, ...props }) => <ol {...props} />,
-                li: ({ node, ...props }) => <li {...props} />,
-                blockquote: ({ node, ...props }) => (
-                  <blockquote
-                    className="mt-6 border-l-2 pl-6 italic"
-                    {...props}
-                  />
-                ),
-                code: ({ node, ...props }) => (
-                  <code
-                    className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold"
-                    {...props}
-                  />
-                ),
-                // TODO: table
-              }}
-            >
-              {article}
-            </ReactMarkdown>
-          </article>
-        ) : (
-          <>
-            {!isLoading ? (
-              <div className="text-center text-gray-500 mt-10">
-                No article content yet. Start a conversation to generate an
-                article.
-              </div>
-            ) : (
-              <div className="flex flex-col gap-2">
-                <Skeleton className="w-1/2 h-10" />
-                <Skeleton className="w-full h-30" />
-                <Skeleton className="w-3/4 h-10" />
-                <Skeleton className="w-full h-15" />
-              </div>
-            )}
-          </>
+        {isClient && (
+          <Tabs value={activeTab} className="h-full">
+            <TabsContent value="markdown" className="h-full">
+              <Textarea
+                className="w-full h-full font-mono resize-none p-4 border-0 ring-0 focus-visible:ring-0"
+                value={article}
+                onChange={handleMarkdownChange}
+                placeholder="Start writing your article here..."
+              />
+            </TabsContent>
+            <TabsContent value="preview" className="h-full">
+              <article className="prose prose-slate dark:prose-invert max-w-none">
+                {article === '' ? (
+                  <div className="text-center text-gray-500 mt-10">
+                    No article content yet. Start a conversation to generate an
+                    article.
+                  </div>
+                ) : (
+                  <ReactMarkdown
+                    remarkPlugins={[remarkGfm]}
+                    rehypePlugins={[rehypeRaw, rehypeSanitize]}
+                    components={{
+                      h1: ({ node, ...props }) => (
+                        <h1
+                          className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl"
+                          {...props}
+                        />
+                      ),
+                      h2: ({ node, ...props }) => (
+                        <h2
+                          className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0"
+                          {...props}
+                        />
+                      ),
+                      h3: ({ node, ...props }) => (
+                        <h3
+                          className="scroll-m-20 text-2xl font-semibold tracking-tight"
+                          {...props}
+                        />
+                      ),
+                      h4: ({ node, ...props }) => (
+                        <h4
+                          className="scroll-m-20 text-xl font-semibold tracking-tight"
+                          {...props}
+                        />
+                      ),
+                      p: ({ node, ...props }) => (
+                        <p
+                          className="leading-7 [&:not(:first-child)]:mt-6"
+                          {...props}
+                        />
+                      ),
+                      ul: ({ node, ...props }) => (
+                        <ul
+                          className="my-6 ml-6 list-disc [&>li]:mt-2"
+                          {...props}
+                        />
+                      ),
+                      ol: ({ node, ...props }) => <ol {...props} />,
+                      li: ({ node, ...props }) => <li {...props} />,
+                      blockquote: ({ node, ...props }) => (
+                        <blockquote
+                          className="mt-6 border-l-2 pl-6 italic"
+                          {...props}
+                        />
+                      ),
+                      code: ({ node, ...props }) => (
+                        <code
+                          className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold"
+                          {...props}
+                        />
+                      ),
+                      // TODO: table
+                    }}
+                  >
+                    {article}
+                  </ReactMarkdown>
+                )}
+              </article>
+            </TabsContent>
+          </Tabs>
         )}
       </div>
     </main>
--- components/dashboard/article-options.tsx
+++ components/dashboard/article-options.tsx
@@ -1,18 +1,36 @@
+import { Button } from '@/components/ui/button'
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { usePanel } from '@/contexts/panel-context'
-import { AppWindowIcon, CodeIcon, Pin, PinOff } from 'lucide-react'
-import { Button } from '../ui/button'
+import {
+  AppWindowIcon,
+  CodeIcon,
+  LoaderCircle,
+  Pin,
+  PinOff,
+} from 'lucide-react'
 import { DialogRemove } from './dialog-remove'
 import { DialogShare } from './dialog-share'
 
-export function ArticleOptions() {
+export function ArticleOptions({
+  activeTab,
+  onTabChange,
+  isLoading,
+}: {
+  activeTab: string
+  onTabChange: (tab: string) => void
+  isLoading: boolean
+}) {
   const { isPinned, pin, unpin } = usePanel()
 
   return (
     <div className="flex justify-between w-full border-b pb-3">
-      <Tabs defaultValue="preview" className="select-none">
+      <Tabs
+        value={activeTab}
+        onValueChange={onTabChange}
+        className="select-none flex flex-row items-center gap-2"
+      >
         <TabsList>
-          <TabsTrigger value="code" disabled>
+          <TabsTrigger value="markdown">
             <CodeIcon />
             Markdown
           </TabsTrigger>
@@ -21,6 +39,7 @@ export function ArticleOptions() {
             Preview
           </TabsTrigger>
         </TabsList>
+        {isLoading && <LoaderCircle className="h-6 w-6 animate-spin" />}
       </Tabs>
 
       <div className="flex gap-2">
